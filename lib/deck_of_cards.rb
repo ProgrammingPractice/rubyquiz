c150fe132b9eb711ede22576b79ffaa3b4147946
Victor Brender <victor@2xwide.com>
2016-02-27 13:01:58 +0200

    [wip] Moving joker B two positions down

--- lib/deck_of_cards.rb
+++ lib/deck_of_cards.rb
@@ -1,5 +1,6 @@
 class DeckOfCards
   JOKER_A = "a"
+  JOKER_B = "b"
 
   def move_joker_A_down(deck)
     i = deck.index(JOKER_A)
@@ -8,5 +9,14 @@ class DeckOfCards
       i = -1
     end
     deck.insert(i + 1, JOKER_A)
+  end
+
+  def move_joker_B_down(deck)
+    i = deck.index(JOKER_B)
+    deck.delete_at(i)
+    if i == deck.size - 1
+      i = -2
+    end
+    deck.insert(i + 2, JOKER_B)
   end
 end
--- spec/deck_of_cards_spec.rb
+++ spec/deck_of_cards_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/deck_of_cards'
 
 describe DeckOfCards do
   let(:j1) { DeckOfCards::JOKER_A }
+  let(:j2) { DeckOfCards::JOKER_B }
 
   let(:deck) { DeckOfCards.new }
 
@@ -12,7 +13,21 @@ describe DeckOfCards do
     end
 
     it "moves the joker A to top of deck when it is the last card" do
-      expect(deck.move_joker_A_down([1, 2, 3, j1])).to eql [j1, 1, 2, 3]
+      expect(deck.move_joker_A_down([1, 2, 3, j1])).to eq [j1, 1, 2, 3]
+    end
+  end
+
+  describe "#move_joker_B_down" do
+    it "moves joker B down two positions" do
+      expect(deck.move_joker_B_down([j2, 1, 2, 3])).to eq [1, 2, j2, 3]
+    end
+
+    it "moves the joker B to top of deck when it is right above the last card" do
+      expect(deck.move_joker_B_down([1, 2, j2, 3])).to eq [j2, 1, 2, 3]
+    end
+
+    it "moves the joker B to second position of deck when it the last card" do
+      expect(deck.move_joker_B_down([1, 2, 3, j2])).to eq [1, j2, 2, 3]
     end
   end
 end
