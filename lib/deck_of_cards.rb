c55f66bf7181cbe7468d4f0090e28d20b260ff29
Florin Oltean <florin@Florin-MBA.fritz.box>
2016-03-12 10:54:43 +0100

    [WIP] Method for tripple cut around the jokers.

--- lib/deck_of_cards.rb
+++ lib/deck_of_cards.rb
@@ -10,6 +10,20 @@ class DeckOfCards
     move_card_down(deck, JOKER_B, 2)
   end
 
+  def triple_cut_around_jokers(deck)
+    i = deck.index(JOKER_A)
+    j = deck.index(JOKER_B)
+    min = [i, j].min
+    max = [i, j].max
+    i = min
+    j = max
+    left = deck[0..i - 1]
+    middle = deck[i..j]
+    right = deck[j + 1..deck.size - 1]
+
+    right + middle + left
+  end
+
   private
 
   def move_card_down(deck, card, positions)
--- spec/deck_of_cards_spec.rb
+++ spec/deck_of_cards_spec.rb
@@ -30,4 +30,14 @@ describe DeckOfCards do
       expect(deck.move_joker_B_down([1, 2, 3, j2])).to eq [1, j2, 2, 3]
     end
   end
+
+  describe "#triple_cut_around_jokers" do
+    it "swaps the cards 'outside' the jokers" do
+      expect(deck.triple_cut_around_jokers([1, 2, j1, 3, j2, 4])).to eq [4, j1, 3, j2, 1, 2]
+    end
+
+    it "does not assume that Joker A is the first one" do
+      expect(deck.triple_cut_around_jokers([1, j2, 2, j1, 3])).to eq [3, j2, 2, j1, 1]
+    end
+  end
 end
