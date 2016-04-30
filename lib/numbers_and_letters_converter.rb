95aa1f00e6ed6be377344f897300437e0680fa84
Victor Brender <victor@2xwide.com>
2016-04-30 11:46:28 +0200

    Fix method call with correct argument

--- lib/numbers_and_letters_converter.rb
+++ lib/numbers_and_letters_converter.rb
@@ -1,5 +1,5 @@
 module NumbersAndLettersConverter
   def self.number_to_letter(number)
-    SolitaireCipher.new(nil).numbers_to_letters([[number]])
+    SolitaireCipher.new(nil).numbers_to_letters([number])
   end
 end
