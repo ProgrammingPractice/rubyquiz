cc50debb0a71ca56185e89045bda20ba5ac9a6f9
Victor Brender <victor@2xwide.com>
2016-04-30 11:59:19 +0200

    Update specs to reflect new code structure.

--- spec/solitaire_cipher_spec.rb
+++ spec/solitaire_cipher_spec.rb
@@ -17,7 +17,7 @@ describe SolitaireCipher do
 
   describe "#prepare_input" do
     it "sanitizes, upcases, splits and pads the input string" do
-      expect(cipher.prepare_input("CodIng\nIn Ru8y;is 4Un! YeY")).to eq "CODIN GINRU YISUN YEYXX"
+      expect(cipher.prepare_input("CodIng\nIn Ru8y;is 4Un! YeY")).to eq "CODINGINRUYISUNYEYXX".split('')
     end
   end
 
@@ -27,35 +27,41 @@ describe SolitaireCipher do
     end
   end
 
-  describe "#split_in_groups_and_pad" do
-    it "creates groups of 5 characters" do
-      expect(cipher.split_in_groups_and_pad('abcdefghij')).to eq 'abcde fghij'
+  describe "#pad_to_multiple_of_5" do
+    it "should add two Xs at the end" do
+      expect(cipher.pad_to_multiple_of_5("abc")).to eq "abcXX"
     end
 
-    it "padds the last group with X's" do
-      expect(cipher.split_in_groups_and_pad('abcdefg')).to eq 'abcde fgXXX'
+    it "should not add any Xs" do
+      expect(cipher.pad_to_multiple_of_5("abcde")).to eq "abcde"
+    end
+  end
+
+  describe "#split_into_groups_of_5" do
+    it "creates groups of 5 characters" do
+      expect(cipher.split_into_groups_of_5('abcdefghij')).to eq 'abcde fghij'
     end
   end
 
   describe "#letters_to_numbers" do
     it "converts the letters to numbers" do
-      expect(cipher.letters_to_numbers('ABCDE XYZ')).to eq [[1, 2, 3, 4, 5], [24, 25, 26]]
+      expect(cipher.letters_to_numbers('ABCDEXYZ'.split(''))).to eq [1, 2, 3, 4, 5, 24, 25, 26]
     end
   end
 
   describe "#numbers_to_letters" do
     it "converts numbers to letters" do
-      expect(cipher.numbers_to_letters([[1, 2, 3, 10, 11], [24, 25, 26]])).to eq 'ABCJK XYZ'
+      expect(cipher.numbers_to_letters([1, 2, 3, 10, 11, 24, 25, 26])).to eq 'ABCJKXYZ'
     end
 
     it "wraps around when alphabet length exceded" do
-      expect(cipher.numbers_to_letters([[27]])).to eq 'A'
+      expect(cipher.numbers_to_letters([27])).to eq 'A'
     end
   end
 
   describe "#add_keys_and_wrap" do
     it "adds keys pairwise and substracts 26 if result is above 26" do
-      expect(cipher.add_keys_and_wrap([[1,2,3,4,5],[20,21]], [[5,4,3,2,1],[6,6]])).to eq [[6,6,6,6,6], [26,1]]
+      expect(cipher.add_keys_and_wrap([1,2,3,4,5,20,21], [5,4,3,2,1,6,6])).to eq [6,6,6,6,6,26,1]
     end
   end
 
